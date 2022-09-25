a9850e94e59fd8079708ba793522ff2950e56d8c
Mohammad Mazraeh <Mazraeh.Mohammad@Gmail.com>
2022-09-24 22:19:38 -0700

    feat: update cdk version, formatting (#1)
    * cleanup repo
    * commit vpn server commands

--- tests/unit/test_aws_cdk_vpn_stack.py
+++ tests/unit/test_aws_cdk_vpn_stack.py
@@ -9,11 +9,11 @@
 # example tests. To run these tests, uncomment this file along with the example
 # resource in aws_cdk_vpn/aws_cdk_vpn_stack.py
 def test_sqs_queue_created():
-#     app = core.App()
-#     stack = AwsCdkVpnStack(app, "aws-cdk-vpn")
-#     template = assertions.Template.from_stack(stack)
+    #     app = core.App()
+    #     stack = AwsCdkVpnStack(app, "aws-cdk-vpn")
+    #     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
-  pass
+    #     template.has_resource_properties("AWS::SQS::Queue", {
+    #         "VisibilityTimeout": 300
+    #     })
+    pass
